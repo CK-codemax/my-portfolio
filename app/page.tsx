180d8a63ea00ebf849c13285e1cef81e46c5ce9d
CK-codemax <ckcodemax@gmail.com>
2024-03-17 08:43:12 +0100

    doing some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,15 @@
-import About from "./components/About";
 import ContactMe from "./Component/ContactMe";
 import Header from "./Component/Header";
 import Hero from "./Component/Hero";
 import Projects from "./Component/Projects";
 import Skills from "./Component/Skills";
-import WorkExperience from "./components/WorkExperience";
-import JumiaClone from "./components/JumiaClone";
-import FaceBookClone from "./components/FaceBookClone";
-import HuluClone from "./components/HuluClone";
-import HuluWeb from "./components/HuluWeb";
+
 import Footer from "./Component/Footer";
 
 
 export default function page() {
   return (
-    // <div className="overflow-y-scroll overflow-x-hidden text-white snap-y snap-mandatory h-screen scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
-    //  <Header />
-    //   <section className="snap-start" id="hero">
-    //     <Hero />
-    //   </section>
-    //   <section className="snap-center" id="about">
-    //     <About />
-    //   </section>
-    //   <section className="snap-center" id="experience">
-    //     <WorkExperience />
-    //   </section>
-
-    //   <section className="snap-center">
-    //     <JumiaClone />
-    //   </section>
-
-    //   <section className="snap-center">
-    //     <HuluClone />
-    //   </section>
-
-    //   <section className="snap-center">
-    //     <FaceBookClone />
-    //   </section>
-
-    //   <section className="snap-center">
-    //     <HuluWeb />
-    //   </section>
-
-    //   <section className="snap-start" id="skills">
-    //     <Skills  />
-    //   </section>
-
-    //   <section className="snap-start" id="projects">
-    //     <Projects  />
-    //   </section>
-
-    //   <section className="snap-start" id="contact">
-    //     <ContactMe  />
-    //   </section>
-    // </div>
-
+    
      <div className="overflow-y-scroll overflow-x-hidden snap-y snap-mandatory h-screen">
         <Header />
         <section className="snap-start" id="hero">
