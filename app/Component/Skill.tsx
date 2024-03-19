2c05e2a42cb23e677356c1e16702090cec76d4a3
CK-codemax <ckcodemax@gmail.com>
2024-03-19 16:18:33 +0100

    made the tech stack image scale transition

--- app/Component/Skill.tsx
+++ app/Component/Skill.tsx
@@ -28,10 +28,10 @@ export default function Skill({percentage, src, name, directionLeft}: Props) {
         <img
         src={src}
         alt="tech-image"
-        className="rounded-full border border-gray-500 object-cover w-[75px] h-[75px] sm:w-28 sm:h-28 lg:h-32 lg:w-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-[75px] h-[75px] group-hover:scale-105 sm:w-28 sm:h-28 lg:h-32 lg:w-32 filter group-hover:grayscale transition-all duration-300 ease-in-out"
         />
 
-        <div className="absolute top-0 lg:translate-x-1 lg:-translate-y-2 left-0 opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-red-500 dark:group-hover:bg-white w-[75px] h-[75px] sm:w-28 sm:h-28 lg:h-32 lg:w-32 rounded-full z-0">
+        <div className="absolute top-0 lg:translate-x-1 lg:-translate-y-2 left-0 opacity-0 group-hover:opacity-80 group-hover:scale-105 transition-all duration-300 ease-in-out group-hover:bg-red-500 dark:group-hover:bg-white w-[75px] h-[75px] sm:w-28 sm:h-28 lg:h-32 lg:w-32 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
                 <p className="text-3xl font-bold text-black opacity-100">{percentage}%</p>
             </div>
